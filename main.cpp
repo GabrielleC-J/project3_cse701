95fb8072c22ceea3fa38f9f94ff26fbc721c800b
GabrielleC-J <gabrielle@cjohnson.ca>
2021-12-05 13:23:39 -0500

    Created the main file which will contain the start of the program

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * @author Gabrielle Ching-Johnson
+ * @brief
+ * @version 0.1
+ * @date Dec 5, 2021
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Testing\n";
+}

5c877a5b68d7668c23154bbff145299899f4a2b8
GabrielleC-J <gabrielle@cjohnson.ca>
2021-12-08 16:09:08 -0500

    added code for constructor that takes a signed integer as an arguement

--- big_int.cpp
+++ big_int.cpp
@@ -16,6 +16,48 @@ big_int::big_int() : coefficient({0}){};
 
 big_int::big_int(const uint64_t &size) : coefficient((size)){};
 
+big_int::big_int(const int64_t &integer)
+{
+    // Add the coefficients/digits for the integer in base 2^32
+    uint64_t value = integer;
+    uint64_t remainder = value % base;
+    coefficient.push_back(remainder);
+    while (value > base)
+    {
+        value = value / base;
+        remainder = value % base;
+        coefficient.push_back(remainder);
+    }
+
+    // Find out if integer is positive or negative
+    if (integer < 0)
+    {
+        integer_sign = sign::NEGATIVE;
+
+        // set the integer to its radix complement
+        uint64_t radix_minus1 = base - 1;
+        // Find radix - 1 complement and then add 1 to get radix complement
+        for (uint64_t &digit : coefficient)
+        {
+            digit = radix_minus1 - digit;
+        }
+        // Add 1 to the big int and check carry
+        uint8_t carry = 0;
+        for (uint64_t &digit : coefficient)
+        {
+            uint64_t temp = digit + 1 + carry;
+            digit = temp & UINT32_MAX; // keep the low 32 bits
+            carry = temp >> 32;        // shift temp to the right by 32 bits to get the carry bit
+        }
+
+        // If the carry is not 0, need to add a new coefficient with the value of carry
+        if (carry != 0)
+        {
+            coefficient.push_back(carry);
+        }
+    }
+}
+
 big_int::big_int(const vector<uint32_t> &vec)
 {
     coefficient.reserve(vec.size());

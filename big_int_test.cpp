be90bc94bd7f208c49ff8d1b17c875830c699e21
GabrielleC-J <gabrielle@cjohnson.ca>
2021-12-09 13:39:04 -0500

    added test cases for the constructors

--- big_int_test.cpp
+++ big_int_test.cpp
@@ -23,7 +23,7 @@ uint8_t failed_counter = 0;
  * @param condition bool indication true if test passed and false if test failed
  * @param file file to write outcome of tests
  */
-void check(bool condition, ofstream file)
+void check(bool condition, ofstream &file)
 {
     if (condition)
     {
@@ -38,6 +38,20 @@ void check(bool condition, ofstream file)
     }
 }
 
+void number_tests_passed(ofstream &file)
+{
+    if (failed_counter == 0)
+    {
+        file << "All " << to_string(passed_counter) << " tests passed!!\n";
+    }
+    else
+    {
+        file << "Not all tests passed!\n"
+             << "Number of tests passed: " << to_string(passed_counter) << "\n"
+             << "Number of tests failed: " << to_string(failed_counter) << "\n";
+    }
+}
+
 /**
  * @brief Prints desired string to a file
  * @param file The file to print line to
@@ -48,6 +62,55 @@ void printline(string line, ofstream file)
 }
 
 /********************************************* Test Functions **********************************************/
+
+/**
+ * @brief Tests to check all constructors
+ */
+void check_constructors(ofstream &file)
+{
+
+    file << "***********Testing all constructors of the big_int class:***********\n";
+
+    // Check the zero constructor
+    file << "Default constructor check, big int set to zero ...";
+    big_int zero_default;
+    check(zero_default.at(0) == 0 && zero_default.coefficient_size() == 1, file);
+
+    // Check constructor with an integer argument
+    file << "\nConstructor checks with Integer argument:\n";
+
+    file << "Integer value less than 2^32 ...";
+    big_int construct_int(4000);
+    check(construct_int.at(0) == 4000 && construct_int.coefficient_size() == 1, file);
+
+    file << "Integer value greater than 2^32 ...";
+    big_int construct_int2(12345678910);
+    check(construct_int2.at(0) == 3755744318 && construct_int2.at(1) == 2 && construct_int2.coefficient_size() == 2, file);
+
+    file << "Negative integer value sign check ...";
+    big_int neg_int(-256);
+    check(neg_int.get_sign() == sign::NEGATIVE, file);
+
+    file << "Negative integer value check coefficients for 2^32 complement ...";
+    check(neg_int.at(0) == 4294967040 && neg_int.coefficient_size() == 1, file);
+
+    // Check constructor with base 10 string argument
+    file << "\nBase 10 string argument constructor checks:\n";
+
+    file << "Check for a positive value (without +) ...";
+    big_int pos_string_int("256");
+    check(pos_string_int.at(0) == 256 && pos_string_int.coefficient_size() == 1, file);
+
+    file << "Check positive string with sign in front ...";
+    check(pos_string_int.at(0) == 256 && pos_string_int.coefficient_size() == 1, file);
+
+    file << "Check for negative value with - in front, that sign flag changes ...";
+    big_int neg_string_int("-256");
+    check(neg_string_int.get_sign() == sign::NEGATIVE, file);
+
+    file << "Check negative string, that coefficients are the 2^32's complement";
+    check(neg_string_int.at(0) == 4294967040 && neg_string_int.coefficient_size() == 1, file);
+}
 
 int main()
 {
@@ -86,19 +149,21 @@ int main()
         cout << "Writing test logs to " << log_file << "\n";
 
     /*********************************** Run tests and Write output to log files ****************************************/
+    check_constructors(log);
+    number_tests_passed(log);
 
     if (failed_counter == 0)
     {
-        log << "All " << to_string(passed_counter) << " tests passed!!\n";
+        log << "\nOverall " << to_string(passed_counter) << " tests passed!!\n";
     }
     else
     {
-        log << "Not all tests passed!\n"
+        log << "\nOverall not all tests passed!\n"
             << "Number of tests passed: " << to_string(passed_counter) << "\n"
             << "Number of tests failed: " << to_string(failed_counter) << "\n";
     }
 
     // Close log file and inform done writing
     log.close();
-    cout << "Finished testing the big_int class, see the log file " << log_file << " for all the information.";
+    cout << "Finished testing the big_int class, see the log file " << log_file << " for all the information.\n";
 }
